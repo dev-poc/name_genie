13cb779144109711a429a72a96a09ea5b032c2f5
dev-poc <82632754+dev-poc@users.noreply.github.com>
2022-01-01 21:39:48 +1000

    Character sets
    Groups of charactersto use together to make cultural names sound different.

---
+++ character_sets.py
@@ -0,0 +1,13 @@
+
+consonants_all=['b','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x','y','z']# 'C' is a bullshit letter, I shan't use it! 
+consonants_hard=['K','B','P','G']# Q is unweildy and annoying, so to avoid the qu convention it's in the too hard basket.
+plosives=['P','B','T','D']# I'm over my head here linguistically :S
+glottal=['g','k']
+sibilant=['f','ch','s','th','h','gh']
+trill=['r','l','zh','z','v','w']
+vowels=['a','e','i','o','u','y']
+dipthongs=['ai','ae','ao','au','ee','oo','ou','ue','oi','ie','ia','ea'] # I assume this is a bit romantic, but my education was quite anglocentric.
+digraphs=['ch', 'ck', 'gh', 'kn', 'mb', 'ng', 'ph', 'sh', 'th', 'wh', 'nd' ,'wr'] # 
+digraphs_non_suffix=['ch', 'ck', 'gh',  'mb', 'ng', 'ph', 'sh', 'th',  'nd' ]
+digraphs_non_prefix=['ch', 'kn', 'ph', 'sh', 'th', 'wh' ,'wr']
+
